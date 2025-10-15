48292a067d9026b428f9b0abb745d60d68da2cd2
bukola-radease <bukola.radease@gmail.com>
2025-10-15 15:48:37 +0100

    my first endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module payment
 
 go 1.25.0
+
+require github.com/stripe/stripe-go/v83 v83.0.1 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,13 +9,59 @@ import (
  func main (){
 	// fmt.Println("hello go")
 	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
-
+		http.HandleFunc("/health", handleHealth)
+	log.Println("..listening on localhost:4242")
 	var err error = http.ListenAndServe("localhost:4242", nil)
 	if err != nil{
 		log.Fatal(err)
 	}
+
 }
 
-	func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request){
-		fmt.Println("endpoint called")
+	func handleCreatePaymentIntent(writer http.ResponseWriter, request *http.Request){
+		// createResponse := [] byte ("you want to create payment?")
+		// _, err := writer.Write(createResponse)
+		// if err !=nil{
+		// 	fmt.Println(err)
+		// }
+		// fmt.Println("endpoint called")
+
+		if request.Method != "POST" {
+			http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+			return
+		}
+		// fmt.Println("request method correct")
+
+
+	var req struct {
+		ProductId string `json:"product_id"`
+		FirstName string `json:"first_name"`
+		LastName string `json:"Last_name"`
+		Address string `json:"address"`
+		City string `json:"city"`
+		Country string `json:"country"`
 	}
+
+err := json.NewDecoder(request.Body).Decode(&req)
+	if err != nil{
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(req.FirstName)
+	fmt.Println(req.LastName)
+		fmt.Println(req.Address)
+			fmt.Println(req.City)
+				fmt.Println(req.Country)
+					fmt.Println(req.ProductId)
+
+}
+
+
+func handleHealth(writer http.ResponseWriter, request *http.Request){
+	response := []byte ("server is up and running")
+	_, err := writer.Write(response)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
